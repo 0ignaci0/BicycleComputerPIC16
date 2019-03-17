a8c7d71cdbf156dd090f46dd02cee7da5e21aff6
0ignaci0 e <riguerra@iupui.edu>
2019-03-17 16:15:56 -0400

    editted display from mph to kmh

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #include "mcc_generated_files/mcc.h"    // MPLAB Configurator; contains initialization routines
 #include "displayOptions.h"             // functions for modifying LCD parameters and writing to display
-#define CIRCUMFERENCE 83             // circumference of 700x23C bicycle wheel in inches
+#define CIRCUMFERENCE 2             // circumference of 700x23C bicycle wheel is 210 cm ; rounded to 2 meters
 
 // constant defines for modifying LCD
 #define configMode      0x7C 
@@ -17,8 +17,9 @@
 long int rpm      = 0 ;
 long int second   = 0 ;
 long int counter  = 0 ;
-long int distance = 0 ;
 long int speed    = 0 ;
+long int distance = 0 ;
+long int km       = 0 ;
 
 void timerISR  ( void ) ;
 void speedCalc ( void ) ;
@@ -56,12 +57,17 @@ void main(void)
     INTERRUPT_PeripheralInterruptEnable();
     while(1){
         setCursor(1,10) ;
-        printf( "%d mi/hr", speed ) ;
-        setCursor(2,11) ;      
-        printf( "%d mi", distance ) ;
-        
-        
-    };
+        printf( "   %d kmh    ", speed ) ;
+        setCursor(2,11) ;
+        if ( distance < 1000 ){
+            printf("   %d m  ", distance ) ;
+        }
+        else if ( distance % 1000 == 0){
+            km++ ;
+            printf("  %d km  ", km ) ;
+        }
+    }
+ 
 }
 
 void timerISR ( void ){ // timer set to 1ms period
@@ -76,11 +82,11 @@ void speedCalc ( void ){
     // calculate rotations per minute by multiplying by 60 sec/min
     rpm      = second * 60 ;    // counter increments every 1 milli-sec, therefore divide by 1000 to achieve seconds
     // calculate speed in mi/hr; speed = ( (2*pi*r) * rpm ) * (60 min/1 hr) * (1 km/1000 m)
-    // below  circum * rpm gives inches per minute ; to get mi/hr, multiply by (1 mi / 63360 inches) * 60 min / 1 hr) = 0.00095
-    speed    = CIRCUMFERENCE * rpm * (0.00095) ; 
+    // below  circum * rpm gives m per min ; to get km/hr, multiply by (1 km / 1000 m) * ( 60 min / 1 hr ) = 6/100 = 3/50 = 0.06
+    speed    = CIRCUMFERENCE * rpm * (0.06) ; // km/hr
     counter  = 0 ;
     second   = 0 ;
-    distance = (distance + CIRCUMFERENCE) * (0.000016) ; // to get miles, multiply by (1 mi/63360 in)
+    distance = distance + CIRCUMFERENCE ; // distance in meters
 }
 
 
